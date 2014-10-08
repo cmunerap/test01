42307374b46530f974a20ff12948b20f261ecdc7
ju4n0perez <ju4n0perez@gmail.com>
2014-10-07 20:42:30 -0500

    Segundo checkin de prueba con Form2

--- WindowsFormsApplication5/Form2.cs
+++ WindowsFormsApplication5/Form2.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication5
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("como estas?");
+        }
     }
 }
